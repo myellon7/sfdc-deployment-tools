f57b8a58a3812d915819dbff7fd6bdf2a8499d96
Mark Yellon <myellon@westmonroepartners.com>
2018-04-09 19:36:19 +0000

    adding functionality to interface directly with google drive API and avoid requirement to download to Excel

--- create.py
+++ create.py
@@ -1,6 +1,26 @@
 ## import xlwings library
 import xlwings as xw
 
+##import dependencies for Google Drive API
+from __future__ import print_function
+from apiclient import discovery
+from httplib2 import Http
+from oauth2client import file, client, tools
+
+##following lines are to set up authentication to your google drive to access
+##changed items log
+SCOPES = 'https://www.googleapis.com/auth/drive.readonly.metadata'
+store = file.Storage('storage.json')
+creds = store.get()
+if not creds or creds.invalid:
+    flow = client.flow_from_clientsecrets('client_secret.json', SCOPES)
+    creds = tools.run_flow(flow, store)
+    
+DRIVE = discovery.build('drive', 'v3', http=creds.authorize(Http()))
+##files = DRIVE.files().list().execute().get('files', [])
+
+
+
 ## instantiate a workbook object
 wb = xw.Book('CT Items Log.xlsx')
 ## gather the worksheet name and data range that will be processed
